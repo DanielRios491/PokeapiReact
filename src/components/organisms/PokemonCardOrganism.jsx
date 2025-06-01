ff3534d592e2863ba87c5bd5e2afd6b68663c51c
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-01 16:11:12 -0400

    refactor: missing element added

--- src/components/organisms/PokemonCardOrganism.jsx
+++ src/components/organisms/PokemonCardOrganism.jsx
@@ -1,5 +1,7 @@
 import PokemonTypeMolecule from "../molecules/PokemonTypeMolecule";
 import InformationMolecule from "../molecules/InformationMolecule";
+import Pokeball from "../../assets/Pokeball.png";
+import Pattern from "../../assets/PatternDrawn.png";
 import '../styles/PokemonCard.css';
 
 export default function PokemonCardOrganism({ id, name, types, image }) {
@@ -7,15 +9,19 @@ export default function PokemonCardOrganism({ id, name, types, image }) {
     return (
         <div className={`pokemon-card ${types[0].toLowerCase()}`}>
             <div className="card-text">
+                <img src={Pattern} alt="Pattern" className="pattern-img" />
+                <img src={Pokeball} alt="Pokeball" className="pokeball-img"/>
                 <InformationMolecule id={id} name={name} />
                 <PokemonTypeMolecule types={types} />
+                <img />
+                <img
+                    src={image}
+                    alt={name}
+                    className="pokemon-image"
+                />
             </div>
 
-            <img
-                src={image}
-                alt={name}
-                className="pokemon-image"
-            />
+
         </div>
     );
 }
