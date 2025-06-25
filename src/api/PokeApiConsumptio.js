908399b817aa5b67a7b4cdeca1d96dcd8c810e9c
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-25 16:59:04 -0400

    It's added display grid in the containers of cards

--- src/api/PokeApiConsumptio.js
+++ src/api/PokeApiConsumptio.js
@@ -1,14 +1,13 @@
-const fetchPokeApiData = async () => {
+export const fetchPokeApiData = async (setPokemons) => {
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0');
         if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        console.log('Fetched Data:', data);
+        setPokemons(data.results)
+        console.log('Fetched Data:', data.results);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 };
-
-fetchPokeApiData();
--- src/components/templates/TestTemplate.jsx
+++ src/components/templates/TestTemplate.jsx
@@ -1,20 +1,42 @@
 import PokemonCardOrganism from '../organisms/PokemonCardOrganism';
+import { fetchPokeApiData } from '../../api/PokeApiConsumptio';
+import { useState, useEffect } from 'react';
 
-const TestTemplate = () => (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <PokemonCardOrganism
-            id="001"
-            name="Bulbasaur"
-            types={["Grass", "Poison"]}
-            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        />
-        <PokemonCardOrganism
-            id="004"
-            name="Charmander"
-            types={["Fire"]}
-            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-        />
-    </div>
-);
+export default function TestTemplate() {
 
-export default TestTemplate;
+    const [ pokemons, setPokemons ] = useState();
+    useEffect(() => {
+        fetchPokeApiData(setPokemons)
+    }, [])
+    
+    
+    return (
+        <div className='tempate-grid'>
+            {
+                pokemons && pokemons.map(pokemon => 
+                    <div key={pokemon.url[pokemon.url.lenght - 2]}>
+                        <PokemonCardOrganism
+                            
+                            id={pokemon.url[pokemon.url.lenght - 2]}
+                            name={pokemon.name}
+                            types={["Grass"]}
+                            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                        />
+                    </div>
+                    )
+            }
+            <PokemonCardOrganism
+                id="001"
+                name="Bulbasaur"
+                types={["Grass", "Poison"]}
+                image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            />
+            <PokemonCardOrganism
+                id="004"
+                name="Charmander"
+                types={["Fire"]}
+                image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+            />
+        </div>
+    );
+}
