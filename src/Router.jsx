34c9deeffb5ce68931ce852e903bdc163309cf66
DanielRiosLopez <Daniel.Rios@jala.university>
2025-07-16 16:46:49 -0400

    feature: changed the route from declarative mode to data mode

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { createBrowserRouter } from "react-router";
 import App from "./App";
 
-export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App
+    },
+]);
+
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router';
 import Router from './Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
