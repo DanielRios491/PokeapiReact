8d7cdc5717a4401ceb8770e3c37b185ee2f0a6e2
DanielRiosLopez <Daniel.Rios@jala.university>
2025-07-28 15:40:54 -0400

    feature: images added

--- src/components/organisms/ImageNavigation.jsx
+++ src/components/organisms/ImageNavigation.jsx
@@ -8,6 +8,12 @@ export default function ImageNavigation({id, type}) {
                 <img src={Pattern} alt="Pattern" className="pattern-img" />
                 <img src={Pattern} alt="Pattern" className="pattern-img" />
             </div>
+            <div className="pokemon-section">
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                    alt="Pokemon" 
+                    loading="lazy"
+                />
+            </div>
         </div>
     );
 }
